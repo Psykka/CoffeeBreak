60b925229e7a7f00f0db8c5b441a82542490223f
Psykka <36780964+Psykka@users.noreply.github.com>
2023-09-19 19:39:27 -0300

    feat: add desktop sizes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,9 @@ import Logo from "../../assets/logo.png";
 
 function Home() {
   return (
-    <section className="flex flex-col justify-center align-middle h-screen bg-beige">
-      <div className="flex flex-col justify-center align-middle p-2 space-y-2">
-        <img className="object-none" src={Logo} alt="Logo" />
+    <section className="flex flex-col justify-around align-middle h-screen bg-beige">
+      <div className="flex justify-center align-middle p-2 space-x-2 my-6 mb-10">
+        <img className="object-none object-center" src={Logo} alt="Logo" />
       </div>
       <h1 className="text-4xl font-sans text-center text-white font-bold">Bem-vindo!</h1>
       <Outlet />
--- src/components/Home/LogIn.jsx
+++ src/components/Home/LogIn.jsx
@@ -6,7 +6,7 @@ function LogIn() {
   return (
     <div className='text-center space-y-4'>
       <div className='flex flex-row justify-center align-middle p-2 space-x-2 my-6 mb-10'>
-        <button className='btn-outline' onClick={() => {
+        <button className='btn-outline w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5' onClick={() => {
           window.location.href = '/signin'
         }}>
           Entrar
@@ -14,7 +14,7 @@ function LogIn() {
       </div>
       <div className='flex flex-row justify-center align-middle p-2 space-x-2'>
         <img className='rotate-180' src={line} />
-        <p className='px-2'>Ou continue com</p>
+        <p className='sm:p-0 md:p-2'>Ou continue com</p>
         <img src={line} />
       </div>
       <div className='flex justify-center align-middle p-2 space-x-2'>
