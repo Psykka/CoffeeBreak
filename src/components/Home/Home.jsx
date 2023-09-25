3331b7a848203ca72082bd75a13a9da236c7dece
Psykka <36780964+Psykka@users.noreply.github.com>
2023-09-24 23:38:48 -0300

    fix: size and add forgotten password

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/logo.png";
 
 function Home() {
   return (
-    <section className="flex flex-col justify-around align-middle h-screen">
+    <section className="flex flex-col justify-around h-screen">
       <div>
         <div className="flex justify-center align-middle p-2 space-x-2 my-6 mb-10">
           <img className="object-none object-center" src={Logo} alt="Logo" />
--- src/components/Home/LogIn.jsx
+++ src/components/Home/LogIn.jsx
@@ -1,12 +1,15 @@
 function LogIn() {
   return (
-    <div className="flex flex-col items-center h-full space-y-4">
-      <form className="flex flex-col items-center mt-16 space-y-10">
-        <div className="flex flex-col items-center space-y-4">
+    <div className="flex flex-col h-screen">
+      <form className="flex flex-col items-center mt-12">
+        <div className="flex flex-col space-y-4 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
           <input className="input-home" type="email" placeholder="E-Mail" />
           <input className="input-home" type="password" placeholder="Senha" />
         </div>
-        <button className='btn-outline w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5'>
+        <p className="text-sm mt-2 text-right text-brown">
+          <a href="#">Esqueci minha senha</a>
+        </p>
+        <button className='btn-outline w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 mt-6'>
           Entrar
         </button>
       </form>
